376466828e5aa9bfa61db8989e2ee6546709132c
Daniel Lazar <daniel@akeo.com>
2023-07-18 09:57:05 +0200

    Added base for vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ciscape/',
   resolve: {
     alias: {
       vue: 'vue/dist/vue.esm-bundler.js',
